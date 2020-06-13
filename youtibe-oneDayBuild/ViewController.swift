657d4fd0c469691acf54b9366af1eacc5e483b6a
AhmedARmohamed <mahmedrashid128@gmail.com>
2020-06-13 18:28:22 +0300

    Initial Xcode Project creation

---
+++ youtibe-oneDayBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtibe-oneDayBuild
+//
+//  Created by Ahmed  on 13/06/2020.
+//  Copyright © 2020 ahmed abdirashid mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
